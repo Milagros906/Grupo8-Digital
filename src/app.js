02e33701a470303bf65e2d25b78c74f6e7f5546f
Rodrigo Beltran <rodrigobeltran777ft@gmail.com>
2023-11-12 23:50:33 -0300

    Rodrigo-1 me había faltado configurar globalmente mi correo y usuario. Yo hice el register.html y su css estiloRegistro.css, añadiendo algunos comentarios para que se entiendan algunas cosas. Espero les guste

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ const path = require ('path');
 
 const publicPath = path.resolve(__dirname, '../public');
 
+// Levantar el servidor
 app.listen(3030, ()=>{
     console.log('Servidor corriendo en el puerto 3030');
 });
 
+// Donde están los recursos estáticos
 app.use(express.static('public'));
 
+// Creando nuestras rutas
 app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
-
