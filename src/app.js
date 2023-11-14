463c5d8e7ed6c2844b55139d9e8aab0ec971b519
Rodrigo Beltran <rodrigobeltran777ft@gmail.com>
2023-11-14 14:00:03 -0300

    Rodrigo-1 ligero cambio, no se por qué se añadio código que no debía

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
-const path = require ('path');
+const path = require('path');
 
 const publicPath = path.resolve(__dirname, '../public');
 
 // Levantar el servidor
-app.listen(3030, ()=>{
+app.listen(3030, () => {
     console.log('Servidor corriendo en el puerto 3030');
 });
 
@@ -18,8 +18,8 @@ app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
 
-app.get('/register', function(req,res){
-    res.sendFile(path.join(__dirname,'/views/register.html'));
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
 })
 
 app.get("/register", function (req, res) {
