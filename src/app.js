1176ef0b1d0f057c5ea127d7234304785d1ca0dc
juanplarr <jp.larrea11@gmail.com>
2023-11-14 16:18:10 -0300

    nuevos cambios en app

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.listen(3030, ()=>{
 });
 
 // Donde están los recursos estáticos
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 // Creando nuestras rutas
 app.get("/", function (req, res) {
